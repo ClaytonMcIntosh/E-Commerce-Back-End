7598085be08d6b7b64fdb26fde8b5c39720fd5f3
Clay <claymcintosh@gmail.com>
2022-03-13 06:45:42 +1000

    updating small bug fixes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -67,23 +67,23 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-  router.delete('/:id', async (req, res) => {
-    try {
-      const categoriesData = await Category.destroy({
-        where: {
-          id: req.params.id
-        }
-      });
-      if (!categoriesData) {
-        res.status(404).json({ message: 'No categories found with this id!' });
-        return;
+  try {
+    const categoriesData = await Category.destroy({
+      where: {
+        id: req.params.id
       }
-      res.status(200).json(categoriesData);
-    } catch (err) {
-      res.status(500).json(err);
+    });
+    if (!categoriesData) {
+      res.status(404).json({ message: 'No categories found with this id!' });
+      return;
     }
-  });
-})
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 module.exports = router;
 
 
